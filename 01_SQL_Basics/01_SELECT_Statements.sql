e03a3763c32c52f364d553d20b18b8d5c4b3547a
metildalouis <metilda.louis16@gmail.com>
2026-06-30 17:18:33 +0530

    Create 01_SELECT_Statements.sql

---
+++ 01_SQL_Basics/01_SELECT_Statements.sql
@@ -0,0 +1,15 @@
+/*
+Topic: SELECT Statement
+*/
+
+-- Display all columns
+
+SELECT *
+FROM employees;
+
+-- Display specific columns
+
+SELECT employee_id,
+       employee_name,
+       salary
+FROM employees;
